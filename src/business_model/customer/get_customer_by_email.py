4aef1762646f5fc4d662ba76bcf439de12bada01
alymohamedhassan <alyhassan10@hotmail.com>
2023-03-04 19:31:13 +0200

    ADD: GetCustomerByEmail Operation

---
+++ src/business_model/customer/get_customer_by_email.py
@@ -0,0 +1,22 @@
+from src.core.business_model import BusinessModel, ModelType
+from src.data_model.customer import Customer
+from src.interface.customer import ICustomer
+
+
+class GetCustomerByEmail(BusinessModel):
+    def __init__(self, email: str):
+        super().__init__(
+            model=Customer(),
+            model_type=ModelType.read
+        )
+
+        self.email = email
+
+    def run(self, data: dict = None, conditions: dict = None) -> dict:
+        return self.model.get_one(
+            condition={
+                "cu_email": {
+                    "$value": str(self.email)
+                }
+            }
+        ).show(True).result
