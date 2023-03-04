de68c01b14ae1629874a79a9722e4ffcede965f7
alymohamedhassan <alyhassan10@hotmail.com>
2023-03-04 19:33:19 +0200

    ADD: SignIn and SignUp Endpoints

--- endpoints/auth.py
+++ endpoints/auth.py
@@ -1,6 +1,12 @@
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 from server.application import app
+from src.business_model.auth.sign_in import SignIn
+from src.business_model.customer.create_customer import CreateCustomer
+from src.business_model.customer.is_customer_exists import IsCustomerExists
+from src.core.response import Response
+from src.interface.customer import ICustomer
 
 
 class ISignIn(BaseModel):
@@ -28,12 +34,46 @@ AUTHENTICATION_TAG = 'Authentication'
 
 @app.post('/auth/signin', tags=[AUTHENTICATION_TAG])
 def signin(user: ISignIn):
-    return user.__dict__
+
+    customer = SignIn(
+        email=user.email,
+        password=user.password,
+    ).run()
+
+    return Response(
+        access_token="",
+        status_code=200,
+        message="",
+        content={
+            "customer": customer
+        },
+    )
 
 
 @app.post('/auth/signup', tags=[AUTHENTICATION_TAG])
 def signup(user: ISignUp):
-    return user.__dict__
+    exists = IsCustomerExists(
+        email=user.email
+    ).run()
+
+    if exists:
+        raise HTTPException(
+            detail="Customer Already Exists",
+            status_code=400
+        )
+
+    customer = CreateCustomer(
+        customer=ICustomer(**user.__dict__)
+    ).run()
+
+    return Response(
+        access_token="",
+        status_code=200,
+        message="",
+        content={
+            "customer": customer
+        },
+    )
 
 
 @app.post('/auth/forget-password', tags=[AUTHENTICATION_TAG])
