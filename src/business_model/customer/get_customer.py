1e0d096f1df6f1751406d60462be782d3da956be
alymohamedhassan <alyhassan10@hotmail.com>
2023-03-04 19:31:07 +0200

    ADD: GetCustomer Operation

---
+++ src/business_model/customer/get_customer.py
@@ -0,0 +1,22 @@
+from src.core.business_model import BusinessModel, ModelType
+from src.data_model.customer import Customer
+from src.interface.customer import ICustomer
+
+
+class GetCustomer(BusinessModel):
+    def __init__(self, customer_id: int):
+        super().__init__(
+            model=Customer(),
+            model_type=ModelType.read
+        )
+
+        self.customer_id = customer_id
+
+    def run(self, data: dict = None, conditions: dict = None) -> dict:
+        return super().run(
+            conditions={
+                "cu_id": {
+                    "$value": str(self.customer_id)
+                }
+            }
+        ).result
