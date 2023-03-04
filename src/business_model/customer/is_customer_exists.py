7006efa08fdde9825efc9a1bfb72ef748370944e
alymohamedhassan <alyhassan10@hotmail.com>
2023-03-04 19:31:23 +0200

    ADD: ICustomerExists Operation

---
+++ src/business_model/customer/is_customer_exists.py
@@ -0,0 +1,21 @@
+from src.business_model.customer.get_customer_by_email import GetCustomerByEmail
+from src.core.business_model import BusinessModel, ModelType
+from src.data_model.customer import Customer
+from src.interface.customer import ICustomer
+
+
+class IsCustomerExists(BusinessModel):
+    def __init__(self, email: str):
+        super().__init__(
+            model=Customer(),
+            model_type=ModelType.read
+        )
+
+        self.email = email
+
+    def run(self, data: dict = None, conditions: dict = None) -> dict:
+        customer = GetCustomerByEmail(
+            email=self.email
+        ).run()
+
+        return len(customer) > 0
