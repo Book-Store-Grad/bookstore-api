94447d863ae7aa4b25fe7b9c6cb9af7b78288d00
alymohamedhassan <alyhassan10@hotmail.com>
2023-03-04 19:35:24 +0200

    Split IsCustomerExists to by Id (Default) and ---ByEmail

--- src/business_model/customer/is_customer_exists_by_email.py
+++ src/business_model/customer/is_customer_exists_by_email.py
@@ -1,22 +1,21 @@
-from src.business_model.customer.get_customer import GetCustomer
 from src.business_model.customer.get_customer_by_email import GetCustomerByEmail
 from src.core.business_model import BusinessModel, ModelType
 from src.data_model.customer import Customer
 from src.interface.customer import ICustomer
 
 
-class IsCustomerExists(BusinessModel):
-    def __init__(self, customer_id: int):
+class IsCustomerExistsByEmail(BusinessModel):
+    def __init__(self, email: str):
         super().__init__(
             model=Customer(),
             model_type=ModelType.read
         )
 
-        self.customer_id = customer_id
+        self.email = email
 
     def run(self, data: dict = None, conditions: dict = None) -> dict:
-        customer = GetCustomer(
-            customer_id=self.customer_id
+        customer = GetCustomerByEmail(
+            email=self.email
         ).run()
 
         return len(customer) > 0
