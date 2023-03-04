355f8c7ef9d7fdb2b318ae048de6d5e2666aebe7
alymohamedhassan <alyhassan10@hotmail.com>
2023-03-04 19:31:38 +0200

    ADD: SignIn BusinessModel

---
+++ src/business_model/auth/sign_in.py
@@ -0,0 +1,21 @@
+from src.business_model.customer.get_customer_by_email import GetCustomerByEmail
+from src.core.business_model import BusinessModel, ModelType
+from src.data_model.customer import Customer
+
+
+class SignIn(BusinessModel):
+    def __init__(self, email: str, password: str):
+        super().__init__(
+            model=Customer(),
+            model_type=ModelType.read,
+        )
+
+        self.email = email
+        self.password = password
+
+    def run(self, data: dict = None, conditions: dict = None) -> dict:
+        customer = GetCustomerByEmail(
+            email=self.email,
+        ).run()
+
+        return customer
