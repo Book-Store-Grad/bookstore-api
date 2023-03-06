61e5572fd763ecd7de949086182744b0872266f3
alymohamedhassan <alyhassan10@hotmail.com>
2023-03-06 13:43:31 +0200

    MODIFY: add customer_id to the condition

--- src/business_model/cart/remove_from_cart.py
+++ src/business_model/cart/remove_from_cart.py
@@ -3,18 +3,19 @@ from src.data_model.cart import CartItem
 
 
 class RemoveFromCart(BusinessModel):
-    def __init__(self, cart_item_id: int):
+    def __init__(self, cart_item_id: int, customer_id: int):
         super().__init__(
             model=CartItem(),
             model_type=ModelType.delete
         )
 
         self.cart_item_id = cart_item_id
+        self.customer_id = customer_id
 
     def run(self, data: dict = None, conditions: dict = None) -> dict:
         return super().run(
-            data={
+            conditions={
                 "cu_id": self.customer_id,
-                "b_id": self.book_id,
+                "b_id": self.cart_item_id,
             }
         ).result
